cd817e5f685bef25e513bb8f2ba5a92480779c0e
Entropy675 <62915307+Entropy675@users.noreply.github.com>
2022-06-12 22:30:34 -0400

    Made Player Interactive Start Screen
    Made a player interactive start screen, the player now has to step on the play button then press either space or enter to start the game.

--- Scenes/StartMenu.gd
+++ Scenes/StartMenu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var selected_menu = 0
+var selected_menu = -1
 
 func change_menu_color():
 	$NewGame.color = Color.gray
@@ -19,16 +19,30 @@ func _ready():
 	change_menu_color()
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("down"):
-		selected_menu = (selected_menu + 1) % 3;
+	#if Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("down"):
+	#if $Quit.get_world_2d().direct_space_state.intersect_point($Player.position, 32, [], 2147483647, true, true):
+	if ($Quit.rect_position + $Quit.rect_size/2).distance_to($Player.position) < 20:
+		selected_menu = 2;
 		change_menu_color()
-	elif Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("up"):
-		if selected_menu > 0:
-			selected_menu = selected_menu - 1
-		else:
-			selected_menu = 2
+		$Enter.visible = true;
+		$Spacebar.visible = true;
+	elif ($LoadGame.rect_position + $LoadGame.rect_size/2).distance_to($Player.position) < 20:
+		selected_menu = 1;
 		change_menu_color()
-	elif Input.is_action_just_pressed("ui_accept"):
+		$Enter.visible = true;
+		$Spacebar.visible = true;
+	elif ($NewGame.rect_position + $NewGame.rect_size/2).distance_to($Player.position) < 20:
+		selected_menu = 0;
+		change_menu_color()
+		$Enter.visible = true;
+		$Spacebar.visible = true;
+	else:
+		selected_menu = -1;
+		change_menu_color()
+		$Enter.visible = false;
+		$Spacebar.visible = false;
+		
+	if Input.is_action_just_pressed("ui_accept"):
 		match selected_menu:
 			0:
 				# New game
@@ -44,3 +58,26 @@ func _input(event):
 			2:
 				# Quit game
 				get_tree().quit()
+
+#	elif Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("up"):
+#		if selected_menu > 0:
+#			selected_menu = selected_menu - 1
+#		else:
+#			selected_menu = 2
+#		change_menu_color()
+#	elif Input.is_action_just_pressed("ui_accept"):
+#		match selected_menu:
+#			0:
+#				# New game
+#				get_tree().change_scene("res://Scenes/Main.tscn")
+#			1:
+#				# Load game
+#				#var next_level_resource = load("res://Scenes/Main.tscn");
+#				#var next_level = next_level_resource.instance()
+#				#next_level.load_saved_game = true
+#				#get_tree().root.call_deferred("add_child", next_level)
+#				#queue_free()
+#				pass
+#			2:
+#				# Quit game
+#				get_tree().quit()
