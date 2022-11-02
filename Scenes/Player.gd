59552281957a62544dc9235d1e935aea751199d2
julianrhoden03 <julianrhoden@hotmail.ca>
2022-11-01 23:10:28 -0400

    Sprint function is attached to energy bar
    If player is less than 20 ep, cannot sprint, sprint bar increases everything second

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -26,89 +26,7 @@ func _ready():
 
 func _physics_process(delta): 
 	
-	#print("x: " + str(position.x) + ", y: " + str(position.y))
-	#print position
 	
-	
-	#middle corn
-	if ((position.x <= 225 and position.x >= 205) and (position.y <= 340 and position.y >= 315)):
-		if Tiles.get_cell(13,20) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(13, 20, -1)
-				acquiredCount.Corn += 1
-	#left corn
-	if ((position.x <= 192 and position.x >= 172) and (position.y <= 337 and position.y >= 317)):
-		if Tiles.get_cell(11,20) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(11, 20, -1)
-				acquiredCount.Corn += 1
-	#right corn
-	if ((position.x <= 257 and position.x >= 237) and (position.y <= 304 and position.y >= 284)):
-		if Tiles.get_cell(15,18) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(15, 18, -1)
-				acquiredCount.Corn += 1
-	#leftappletree
-	if ((position.x <= 137 and position.x >= 117) and (position.y <= 225 and position.y >= 205)):
-		if Tiles.get_cell(7,12) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(7, 12, -1)
-				acquiredCount.Apple += 1
-	#rightappletree
-	if ((position.x <= 537 and position.x >= 517) and (position.y <= 95 and position.y >= 75)):
-		if Tiles.get_cell(32,4) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(32, 4, -1)
-				acquiredCount.Apple += 1
-	#leftraspberry
-	if ((position.x <= 209 and position.x >= 189) and (position.y <= 308 and position.y >= 288)):
-		if Tiles.get_cell(12,18) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(12, 18, -1)
-				acquiredCount.Rasp += 1
-	#rightraspberry
-	if ((position.x <= 271 and position.x >= 251) and (position.y <= 320 and position.y >= 300)):
-		if Tiles.get_cell(16,19) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(16, 19, -1)
-				acquiredCount.Rasp += 1
-	#leftRedMush
-	if ((position.x <= 437 and position.x >= 417) and (position.y <= 210 and position.y >= 190)):
-		if Tiles.get_cell(26,12) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(26, 12, -1)
-				acquiredCount.RedMush += 1
-	#rightRedMush
-	if ((position.x <= 496 and position.x >= 476) and (position.y <= 182 and position.y >= 172)):
-		if Tiles.get_cell(30,11) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(30, 11, -1)
-				acquiredCount.RedMush += 1
-	#PurpMush
-	if ((position.x <= 161 and position.x >= 141) and (position.y <= 256 and position.y >= 236)):
-		if Tiles.get_cell(9,15) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(9, 15, -1)
-				acquiredCount.PurpleMush += 1
-	#Blue1
-	if ((position.x <= 145 and position.x >= 125) and (position.y <= 305 and position.y >= 285)):
-		if Tiles.get_cell(8,18) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(8, 18, -1)
-				acquiredCount.BlueBerry += 1
-	#Blue2
-	if ((position.x <= 561 and position.x >= 541) and (position.y <= 206 and position.y >= 186)):
-		if Tiles.get_cell(34,12) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(34, 12, -1)
-				acquiredCount.BlueBerry += 1
-	#Blue3
-	if ((position.x <= 579 and position.x >= 559) and (position.y <= 224 and position.y >= 204)):
-		if Tiles.get_cell(35,13) != -1:
-			if Input.is_action_just_pressed("press_enter"):
-				Tiles.set_cell(35, 13, -1)
-				acquiredCount.BlueBerry += 1
-		
 			
 	
 	motion.x = lerp(motion.x, 0, 0.2);
@@ -116,14 +34,108 @@ func _physics_process(delta):
 	motion.y = clamp(motion.y, -MAXSPEED, MAXSPEED);
 	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED);
 	
-	#if Energybar.value > 20:
-	if Input.is_action_pressed("sprint") and !dashing and startDash == -1:
-		ACCEL = 210
-		startDash = 2
-		Energybar.value -= 20
+	if get_tree().get_current_scene().get_name() == "StartScreen":
+		if Input.is_action_pressed("sprint") and !dashing and startDash == -1:
+			ACCEL = 210
+			startDash = 2
+		
 			
 	
 	if(get_tree().get_current_scene().get_name() == "Main"):
+		
+		#print("x: " + str(position.x) + ", y: " + str(position.y))
+	#print position
+		
+		if Input.is_action_pressed("sprint") and !dashing and startDash == -1:
+			if Energybar.value >= 20:
+				ACCEL = 210
+				startDash = 2
+				Energybar.value -= 20
+	
+	#middle corn
+		if ((position.x <= 225 and position.x >= 205) and (position.y <= 340 and position.y >= 315)):
+			if Tiles.get_cell(13,20) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(13, 20, -1)
+					acquiredCount.Corn += 1
+		#left corn
+		if ((position.x <= 192 and position.x >= 172) and (position.y <= 337 and position.y >= 317)):
+			if Tiles.get_cell(11,20) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(11, 20, -1)
+					acquiredCount.Corn += 1
+		#right corn
+		if ((position.x <= 257 and position.x >= 237) and (position.y <= 304 and position.y >= 284)):
+			if Tiles.get_cell(15,18) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(15, 18, -1)
+					acquiredCount.Corn += 1
+		#leftappletree
+		if ((position.x <= 137 and position.x >= 117) and (position.y <= 225 and position.y >= 205)):
+			if Tiles.get_cell(7,12) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(7, 12, -1)
+					acquiredCount.Apple += 1
+		#rightappletree
+		if ((position.x <= 537 and position.x >= 517) and (position.y <= 95 and position.y >= 75)):
+			if Tiles.get_cell(32,4) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(32, 4, -1)
+					acquiredCount.Apple += 1
+		#leftraspberry
+		if ((position.x <= 209 and position.x >= 189) and (position.y <= 308 and position.y >= 288)):
+			if Tiles.get_cell(12,18) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(12, 18, -1)
+					acquiredCount.Rasp += 1
+		#rightraspberry
+		if ((position.x <= 271 and position.x >= 251) and (position.y <= 320 and position.y >= 300)):
+			if Tiles.get_cell(16,19) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(16, 19, -1)
+					acquiredCount.Rasp += 1
+		#leftRedMush
+		if ((position.x <= 437 and position.x >= 417) and (position.y <= 210 and position.y >= 190)):
+			if Tiles.get_cell(26,12) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(26, 12, -1)
+					acquiredCount.RedMush += 1
+		#rightRedMush
+		if ((position.x <= 496 and position.x >= 476) and (position.y <= 182 and position.y >= 172)):
+			if Tiles.get_cell(30,11) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(30, 11, -1)
+					acquiredCount.RedMush += 1
+		#PurpMush
+		if ((position.x <= 161 and position.x >= 141) and (position.y <= 256 and position.y >= 236)):
+			if Tiles.get_cell(9,15) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(9, 15, -1)
+					acquiredCount.PurpleMush += 1
+		#Blue1
+		if ((position.x <= 145 and position.x >= 125) and (position.y <= 305 and position.y >= 285)):
+			if Tiles.get_cell(8,18) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(8, 18, -1)
+					acquiredCount.BlueBerry += 1
+		#Blue2
+		if ((position.x <= 561 and position.x >= 541) and (position.y <= 206 and position.y >= 186)):
+			if Tiles.get_cell(34,12) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(34, 12, -1)
+					acquiredCount.BlueBerry += 1
+		#Blue3
+		if ((position.x <= 579 and position.x >= 559) and (position.y <= 224 and position.y >= 204)):
+			if Tiles.get_cell(35,13) != -1:
+				if Input.is_action_just_pressed("press_enter"):
+					Tiles.set_cell(35, 13, -1)
+					acquiredCount.BlueBerry += 1
+		
+		
+		
+		
+		
+		
 		if ($"../CanvasLayer/Interface/Bars/LifeBar/TextureProgress".value <= 0):
 			get_tree().change_scene("res://Scenes/GameOver.tscn")
 	
